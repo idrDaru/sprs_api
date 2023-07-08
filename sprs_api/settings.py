c5f8295320ea871af84ea8570700f1222b245242
imam <idrdaru@gmail.com>
2023-07-08 08:59:07 +0800

    vercel deployment configuration

--- sprs_api/settings.py
+++ sprs_api/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.100.24.75', '127.0.0.1', '192.168.72.46']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
