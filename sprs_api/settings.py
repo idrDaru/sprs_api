3577a5bc8a0ef71f82c9d95f490e8cf51385da2a
imam <idrdaru@gmail.com>
2023-07-11 07:50:17 +0800

    push venv

--- sprs_api/settings.py
+++ sprs_api/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '10.60.145.10']
 
 
 # Application definition
