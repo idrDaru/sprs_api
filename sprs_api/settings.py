49316fb619e1784d65314fededb288154ebe9977
imam <idrdaru@gmail.com>
2023-07-11 09:41:35 +0800

    change allowed host

--- sprs_api/settings.py
+++ sprs_api/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '10.60.145.10']
+ALLOWED_HOSTS = ['0.0.0.0', '.vercel.app']
 
 
 # Application definition
